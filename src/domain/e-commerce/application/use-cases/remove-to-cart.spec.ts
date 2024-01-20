d5b1573c9afd4666a968ecf201ac874dccab56f6
erikarosah <erikarosamg@gmail.com>
2024-01-20 19:32:29 -0300

    Test: remove to cart use case

---
+++ src/domain/e-commerce/application/use-cases/remove-to-cart.spec.ts
@@ -0,0 +1,97 @@
+import { describe, beforeEach, it, expect } from 'vitest'
+import { InMemoryProductRepository } from 'test/repositories/in-memory-product-repository'
+import { RemoveToCartUseCase } from './remove-to-cart'
+import { InMemoryUserRepository } from 'test/repositories/in-memory-user-repository'
+import { User } from '../../entities/user'
+import { Product } from '../../entities/product'
+import { AddToCartUseCase } from './add-to-cart'
+import { UniqueID } from '@/core/entities/unique-id'
+
+let inMemoryUserRepository: InMemoryUserRepository
+let inMemoryProductRepositoty: InMemoryProductRepository
+let addToCartUseCase: AddToCartUseCase
+let sut: RemoveToCartUseCase
+
+describe('Remove to Cart Use Case', () => {
+    beforeEach(() => {
+        inMemoryUserRepository = new InMemoryUserRepository
+        inMemoryProductRepositoty = new InMemoryProductRepository
+        sut = new RemoveToCartUseCase(inMemoryUserRepository, inMemoryProductRepositoty)
+    })
+
+    it('should be able to remove a product to cart', async () => {
+        addToCartUseCase = new AddToCartUseCase(inMemoryUserRepository, inMemoryProductRepositoty)
+
+        const user = User.create({
+            name: 'some name',
+            email: 'some email',
+            password: 'some password'
+        })
+
+        inMemoryUserRepository.create(user)
+
+        const product = Product.create({
+            name: 'some product',
+            available: true,
+            category: 'some category',
+            image: 'url',
+            new_price: 50,
+            old_price: 40
+        })
+
+        inMemoryProductRepositoty.create(product)
+
+        await addToCartUseCase.execute({
+            productId: product.id.toString(),
+            userId: user.id.toString()
+        })
+
+        await sut.execute({
+            productId: product.id.toString(),
+            userId: user.id.toString()
+        })
+
+        expect(inMemoryUserRepository.items[0].cart).toHaveLength(0)
+    })
+
+    it('should be able to remove several products to cart', async () => {
+        addToCartUseCase = new AddToCartUseCase(inMemoryUserRepository, inMemoryProductRepositoty)
+
+        const user = User.create({
+            name: 'some name',
+            email: 'some email',
+            password: 'some password'
+        })
+
+        inMemoryUserRepository.create(user)
+
+        for (let i = 1; i <= 5; i++) {
+            inMemoryProductRepositoty.create(
+                Product.create({
+                    name: `product ${i}`,
+                    available: true,
+                    category: 'some category',
+                    image: 'url',
+                    new_price: 50,
+                    old_price: 40
+                },
+                    new UniqueID(`${i}`)
+                )
+            )
+        }
+
+        for (let i = 1; i <= 5; i++) {
+            await addToCartUseCase.execute({
+                productId: `${i}`,
+                userId: user.id.toString()
+            })
+        }
+
+        await sut.execute({
+            productId: '1',
+            userId: user.id.toString()
+        })
+
+        expect(inMemoryUserRepository.items[0].cart).toHaveLength(4)
+    })
+})
