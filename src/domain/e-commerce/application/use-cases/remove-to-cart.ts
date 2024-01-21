c7e8d9a0cde2b03d6383b696200146057db46ec1
erikarosah <erikarosamg@gmail.com>
2024-01-20 22:09:18 -0300

    Feat: new remove to card use case

---
+++ src/domain/e-commerce/application/use-cases/remove-to-cart.ts
@@ -0,0 +1,39 @@
+import { ProductRepository } from '../repositories/product-repository'
+import { UserRepository } from '../repositories/user-repository'
+
+interface RemoveToCartUseCaseRequest {
+    userId: string,
+    productId: string
+}
+
+interface RemoveToCartUseCaseResponse { }
+
+export class RemoveToCartUseCase {
+    constructor(
+        private userRepository: UserRepository,
+        private productRepository: ProductRepository
+    ) { }
+
+    async execute({
+        userId,
+        productId
+    }: RemoveToCartUseCaseRequest): Promise<RemoveToCartUseCaseResponse> {
+
+        const product = await this.productRepository.findById(productId)
+        const user = await this.userRepository.findById(userId)
+
+        if (product && user) {
+            const productOnCart = user.cart.find((item) => item.id === product.id)
+            const productIndex = user.cart.findIndex((item) => item.id === product.id)
+
+            if (productOnCart) {
+                user.cart.splice(productIndex, 1)
+
+            } else {
+                throw new Error('Resource not found')
+            }
+        }
+
+        return {}
+    }
+}
