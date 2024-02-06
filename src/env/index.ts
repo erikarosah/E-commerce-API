bfbf18b860ce8f74a1e84f31f050ab3e0688c951
erikarosah <erikarosamg@gmail.com>
2024-02-06 13:34:26 -0300

    Refactor: env file

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,14 +1,18 @@
 import 'dotenv/config'
 import { z } from 'zod'
 
+const SECRET = process.env.SECRET
+const STRIPE_API = process.env.STRIPE_API
+const URL_FRONT = process.env.URL_FRONT
+
 const envSchema = z.object({
     NODE_ENV: z.enum(
         ['dev', 'test', 'production']
     ).default('dev'),
     PORT: z.coerce.number().default(3333),
-    SECRET: z.string(),
-    STRIPE_API: z.string(),
-    URL_FRONT: z.string()
+    SECRET: z.string().default(SECRET ? SECRET : ''),
+    STRIPE_API: z.string().default(STRIPE_API ? STRIPE_API : ''),
+    URL_FRONT: z.string().default(URL_FRONT ? URL_FRONT : '')
 })
 
 const _env = envSchema.safeParse(process.env)
