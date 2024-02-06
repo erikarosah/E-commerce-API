906218f4d55788e1a9b979a25ef807747cd748fc
erikarosah <erikarosamg@gmail.com>
2024-02-06 12:36:30 -0300

    Refactor: install pm2 v2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'app',
-    script: './build/server.js',
+    script: 'build/server.js',
     instances: 'max',
     env: {
       NODE_ENV: 'development',
